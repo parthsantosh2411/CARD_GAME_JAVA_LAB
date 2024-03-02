8696ef15de5ff48274a050cb9489407350ac502f
Parth Santosh Tripathi <114149533+parthsantosh2411@users.noreply.github.com>
2024-03-02 11:48:24 +0530

    Add files via upload

---
+++ Card.java
@@ -0,0 +1,44 @@
+public class Card {
+    public static final String[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+    public static final String[] ranks = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen",
+            "King" };
+
+    private int rank;
+    private String suit;
+
+    public Card(int rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public String toString() {
+        return ranks[rank - 1] + " of " + suit;
+    }
+
+    public int compareTo(Card other) {
+        if (this.rank != other.rank) {
+            return Integer.compare(this.rank, other.rank);
+        } else {
+            return this.suit.compareTo(other.suit);
+        }
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Card other = (Card) obj;
+        return rank == other.rank && suit.equals(other.suit);
+    }
+}
